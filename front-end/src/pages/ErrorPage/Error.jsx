0525f4b2e7ff0cf343538a0d4b5ba37399eb4fab
RudraSarkar5 <rudrasarkar311@gmail.com>
2024-02-15 11:52:54 +0530

    fix error page

--- front-end/src/pages/ErrorPage/Error.jsx
+++ front-end/src/pages/ErrorPage/Error.jsx
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router";
 
 const Error = () => {
   
-
+  const navigate = useNavigate();
+ 
+  const handleBack=()=>{
+     navigate(-1);
+  }
+  
   return (
     <div className="h-screen w-screen px-2 flex flex-col justify-center items-center bg-gradient-to-b from-red-500 to-red-700 text-white">
       <div className="text-4xl font-bold mb-4">Oops, Something Went Wrong!</div>
@@ -13,9 +19,9 @@ const Error = () => {
       </p>
       <button
         className="mt-8 px-4 py-2 bg-white text-red-500 rounded-lg hover:bg-red-100 hover:text-red-600 focus:outline-none"
-        
+        onClick={handleBack}
       >
-        Reload Page
+        Back
       </button>
     </div>
   );
