afc1936a90814618901bff85bd336b8759d6ee8f
RudraSarkar5 <rudrasarkar311@gmail.com>
2024-02-26 08:38:43 +0530

    added chat api's

--- back-end/controlers/chat.controler.js
+++ back-end/controlers/chat.controler.js
@@ -1,11 +1,77 @@
 import { chatModel } from "../models/chat.model.js"
+import UserModel  from "../models/user.model.js"
 
-export const getFriends=(req,res)=>{
+export const getFriends=async (req,res)=>{
+    const {userId} = req.params;
+    const allFriendsId = (await chatModel.find({participant : {$in : [userId]}})).flatMap(({participant})=>participant.filter((id)=>id != userId))
+    const correspondingFriends = await UserModel.find({
+      _id: { $in: allFriendsId }
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "fetched all friends",
+      friends : correspondingFriends
+    });
+}
+
+export const getMessage=async (req,res)=>{
+    const {senderId,receiverId} = req.query;
+    const conversation = await chatModel.findOne({"participant" : {$all:[senderId,receiverId]}});
+    if(conversation){
+        return res.status(200).json({
+          success: true,
+          messages : conversation.messages
+        });
+    }else{
+        return res.status(200).json({
+          success: true,
+          messages: []
+        });
+    }
     
 }
-export const getMessage=(req,res)=>{
 
-}
-export const addMessage=(req,res)=>{
 
-}
+export const addMessage = async (req, res) => {
+  console.log(req.body);
+  const { senderId, receiverId, message } = req.body;
+  const messageDocument = {
+    senderId,
+    content: message,
+  };
+
+  try {
+    let conversation = await chatModel.findOne({
+      participant: { $all: [senderId, receiverId] }
+    });
+
+    if (conversation) {
+      // Conversation exists, add message to existing conversation
+      conversation.messages.push(messageDocument);
+      await conversation.save();
+      return res.status(200).json({
+        success: true,
+        conversation
+      });
+    } else {
+      // Conversation does not exist, create a new conversation
+      conversation = await chatModel.create({
+        participant: [senderId, receiverId],
+        messages: [messageDocument]
+      });
+
+      return res.status(200).json({
+        success: true,
+        conversation
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      success: false,
+    });
+  }
+};
+
+
--- back-end/routes/chat.route.js
+++ back-end/routes/chat.route.js
@@ -3,7 +3,7 @@ import {getFriends,getMessage,addMessage} from "../controlers/chat.controler.js"
 
 const chatRouter = Router();
 
-chatRouter.get("/get-friends",getFriends);
+chatRouter.get("/get-friends/:userId",getFriends);
 chatRouter.post("/add-message",addMessage);
 chatRouter.get("/get-message",getMessage);
 
