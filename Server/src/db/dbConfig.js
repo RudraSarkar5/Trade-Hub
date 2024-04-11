ac0abc4b38d41dd778630f1bb542631833d0cc3a
RudraSarkar5 <rudrasarkar311@gmail.com>
2024-04-11 14:01:26 +0530

    modified database connection logic

--- Server/src/db/dbConfig.js
+++ Server/src/db/dbConfig.js
@@ -3,18 +3,29 @@ import { DB_NAME } from "../constants.js";
 
 // this function will connnect databases
 const dbconnection = async () => {
+
   try {
-    const mongoDbInstance = await mongoose.connect(
+
+    mongoose.connection.on("connected",()=>{
+      console.log("database connected successfully.");
+    })
+
+    mongoose.connection.on("disconnected",()=>{
+      console.log("database connection failled");
+      process.exit(1);
+    })
+
+    await mongoose.connect(
       `${process.env.MONGO_DB_URL}/${DB_NAME}`
     );
-
-    if (mongoDbInstance) {
-      console.log(`db connected at ${mongoDbInstance.connection.host} `);
-    }
+    
   } catch (error) {
-    console.log("MongoDb connection ERROR : ", error);
-     process.exit(1);
+
+    console.log("MongoDb connection ERROR : ", error.message);
+    process.exit(1);
+
   }
+
 };
 
 export default dbconnection;
